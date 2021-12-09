98bcde92fc27b2cc6e25a89ef35957f587e341f2
Dylan <dylan.daev@gmail.com>
2021-12-08 21:50:50 -0600

    Terminado sign up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 const nombreInput = document.getElementById("nombre");
+const correoInput = document.getElementById("correo");
 const contraInput = document.getElementById("contraseña");
-const loginBtn = document.getElementById("login-btn");
+const repeatInput = document.getElementById("repeat");
 
-document.querySelectorAll("form.col-1 > .row").forEach((row) => {
+const loginBtn = document.querySelector(".btn.print");
+
+const rojoPalido = "#dc7c7c";
+const white = "#ffffff";
+
+document.querySelectorAll(".col > .row").forEach((row) => {
   let input = row.children[0];
   let label = row.children[1];
 
   input.addEventListener("blur", () => {
     if (input.value) {
       label.classList.remove("animated-up");
-    } else if (!label.classList.contains("animated-up")) {
-      label.classList.add("animated-up");
+
+      if (label.classList.contains("required")) {
+        label.style.color = white;
+
+        input.classList.remove("rotate");
+      }
+    } else {
+      if (!label.classList.contains("animated-up")) {
+        label.classList.add("animated-up");
+      }
+      if (label.classList.contains("required")) {
+        label.style.color = rojoPalido;
+
+        input.classList.add("rotate");
+      }
     }
   });
 });
@@ -19,5 +38,19 @@ loginBtn.addEventListener("click", () => {
   let nombre = nombreInput.value;
   let contraseña = contraInput.value;
 
-  if (nombre && contraseña) console.log({ nombre, contraseña });
+  if (nombre && contraseña) {
+    if (correoInput && repeatInput) {
+      let correo = correoInput.value;
+      let repeat = repeatInput.value;
+
+      console.log({
+        nombre,
+        contraseña,
+        correo,
+        "verificacion correo": repeat,
+      });
+    } else {
+      console.log({ nombre, contraseña });
+    }
+  }
 });
