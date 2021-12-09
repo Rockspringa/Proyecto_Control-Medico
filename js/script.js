1117074c811efb0695bea04b5036e1da74d29686
Dylan <dylan.daev@gmail.com>
2021-12-09 00:53:29 -0600

    Terminado ingresos

--- js/script.js
+++ js/script.js
@@ -3,14 +3,30 @@ const correoInput = document.getElementById("correo");
 const contraInput = document.getElementById("contraseña");
 const repeatInput = document.getElementById("repeat");
 
-const loginBtn = document.querySelector(".btn.print");
+const cuiClienteInput = document.getElementById("cui");
+const deptoClienteInput = document.getElementById("depto");
+const nombreClienteInput = document.getElementById("nombre-c");
+const apellidoClienteInput = document.getElementById("apellido-c");
+
+const printBtn = document.querySelector(".btn.print");
+const printClienteBtn = document.querySelector(".btn.print-cliente");
 
 const rojoPalido = "#dc7c7c";
 const white = "#ffffff";
 
+function doShake(input, label) {
+  if (!input.value) {
+    if (label.classList.contains("required")) {
+      label.style.color = rojoPalido;
+
+      input.classList.add("rotate");
+    }
+  }
+}
+
 document.querySelectorAll(".col > .row").forEach((row) => {
-  let input = row.children[0];
-  let label = row.children[1];
+  const input = row.children[0];
+  const label = row.children[1];
 
   input.addEventListener("blur", () => {
     if (input.value) {
@@ -25,32 +41,56 @@ document.querySelectorAll(".col > .row").forEach((row) => {
       if (!label.classList.contains("animated-up")) {
         label.classList.add("animated-up");
       }
-      if (label.classList.contains("required")) {
-        label.style.color = rojoPalido;
 
-        input.classList.add("rotate");
-      }
+      doShake(input, label);
     }
   });
 });
 
-loginBtn.addEventListener("click", () => {
-  let nombre = nombreInput.value;
-  let contraseña = contraInput.value;
+if (printBtn) {
+  printBtn.addEventListener("click", () => {
+    const nombre = nombreInput.value;
+    const contraseña = contraInput.value;
 
-  if (nombre && contraseña) {
-    if (correoInput && repeatInput) {
-      let correo = correoInput.value;
-      let repeat = repeatInput.value;
+    if (nombre && contraseña) {
+      if (correoInput && repeatInput) {
+        const correo = correoInput.value;
+        const repeat = repeatInput.value;
 
-      console.log({
-        nombre,
-        contraseña,
-        correo,
-        "verificacion correo": repeat,
-      });
-    } else {
-      console.log({ nombre, contraseña });
+        console.log({
+          nombre,
+          contraseña,
+          correo,
+          "verificacion correo": repeat,
+        });
+      } else {
+        console.log({ nombre, contraseña });
+      }
     }
-  }
-});
+
+    document.querySelectorAll(".col > .row").forEach((row) => {
+      const input = row.children[0];
+      const label = row.children[1];
+
+      doShake(input, label);
+    });
+  });
+} else if (printClienteBtn) {
+  printClienteBtn.addEventListener("click", () => {
+    const nombre = nombreClienteInput.value;
+    const apellido = apellidoClienteInput.value;
+    const cui = cuiClienteInput.value;
+    const departamento = deptoClienteInput.value;
+
+    if (nombre && apellido && cui && departamento) {
+      console.log({ nombre, apellido, cui, departamento });
+    } else {
+      document.querySelectorAll(".col > .row").forEach((row) => {
+        const input = row.children[0];
+        const label = row.children[1];
+
+        doShake(input, label);
+      });
+    }
+  });
+}
